3c4fbafa54d799ac496c5dce53bbd3fa2ea8b019
Stephanie Bowles <sbowl001@fiu.edu>
2019-05-30 21:42:42 -0400

    login view init

---
+++ Gigs/Gigs/LoginViewController.swift
@@ -0,0 +1,38 @@
+//
+//  LoginViewController.swift
+//  Gigs
+//
+//  Created by Stephanie Bowles on 5/30/19.
+//  Copyright © 2019 Stephanie Bowles. All rights reserved.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+
+    
+    @IBOutlet weak var signSegmentedControl: UISegmentedControl!
+    
+    @IBOutlet weak var usernameField: UITextField!
+    
+    @IBOutlet weak var passwordField: UITextField!
+    
+    @IBOutlet weak var signButton: UIButton!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+       
+    }
+    
+    @IBAction func signingTapped(_ sender: UISegmentedControl) {
+    }
+    
+
+    @IBAction func signButtonTapped(_ sender: UIButton) {
+    }
+    // MARK: - Navigation
+
+  
+
+}
